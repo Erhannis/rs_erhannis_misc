e663aeb7c4a5ebdbf3579ecd52e7301bb2670947
erhannis <eyeillus@gmail.com>
2026-02-07 18:29:35 -0500

    Added no_std feature gate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub mod autodrop_thread;
+pub mod rate_meter;
 pub mod autotimer;
+
+#[cfg(feature = "std")]
+pub mod autodrop_thread;
+#[cfg(feature = "std")]
 pub mod unbounded_broadcast;
---
+++ src/rate_meter.rs
@@ -0,0 +1,128 @@
+
+//MISC Ugh, I don't like all the std/nostd back and forth
+
+#[cfg(feature = "std")]
+use std::time::{Instant, Duration};
+
+// From esp_hal
+#[cfg(not(feature = "std"))]
+type Instant = fugit::Instant<u64, 1, 1_000_000>;
+#[cfg(not(feature = "std"))]
+type Duration = fugit::Duration<u64, 1, 1_000_000>;
+
+pub struct RateMeter {
+  pub count: u64,
+  pub last_time: Instant,
+  pub interval: Duration,
+}
+
+impl RateMeter {
+  pub fn new(
+    #[cfg(not(feature = "std"))]
+    now: Instant,
+  ) -> RateMeter {
+    #[cfg(feature = "std")]
+    let now = Instant::now();
+
+    #[cfg(not(feature = "std"))]
+    let interval = Duration::millis(1000);
+    
+    #[cfg(feature = "std")]
+    let interval = Duration::from_millis(1000);
+
+    RateMeter {
+      count: 0,
+      last_time: now,
+      interval,
+    }
+  }
+
+  /**
+   * Adds n to the count.
+   */
+  pub fn add(&mut self, n: u64) {
+    self.count += n;
+  }
+
+  /**
+   * Adds 1 to the count.
+   */
+  pub fn inc(&mut self) {
+    self.count += 1;
+  }
+
+  /**
+   * Adds 1 to the count.  Checks time interval.  If elapsed, reset and return rate (count / second) since last reset.
+   */
+  pub fn auto(
+    &mut self,
+    #[cfg(not(feature = "std"))]
+    now: Instant,
+  ) -> Option<f64> {
+    self.inc();
+    return self.check(
+      #[cfg(not(feature = "std"))]
+      now,
+    );
+  }
+
+  /**
+   * Checks time interval.  If elapsed, reset and return rate (count / second) since last reset.
+   */
+  pub fn check(
+    &mut self,
+    #[cfg(not(feature = "std"))]
+    now: Instant,
+  ) -> Option<f64> {
+    #[cfg(feature = "std")]
+    let now = Instant::now();
+
+    if now >= self.last_time + self.interval {
+      #[cfg(feature = "std")]
+      let r = Some((self.count as f64) / (now - self.last_time).as_secs_f64());
+
+      #[cfg(not(feature = "std"))]
+      let r = {
+        //LEAK It would be nice to support nanos without losing precision for large times
+        let us = (now - self.last_time).to_micros();
+        let s = (us as f64) / 1_000_000_f64;
+        let r = Some((self.count as f64) / s);
+        r
+      };
+
+      self.last_time = now;
+      self.count = 0;
+      return r;
+    } else {
+      return None;
+    }
+  }
+
+  /**
+   * Reset and return rate (count / second) since last reset.
+   */
+  pub fn measure(
+    &mut self,
+    #[cfg(not(feature = "std"))]
+    now: Instant,
+  ) -> f64 {
+    #[cfg(feature = "std")]
+    let now = Instant::now();
+
+    #[cfg(feature = "std")]
+    let r = (self.count as f64) / (now - self.last_time).as_secs_f64();
+
+    #[cfg(not(feature = "std"))]
+    let r = {
+      //LEAK It would be nice to support nanos without losing precision for large times
+      let us = (now - self.last_time).to_micros();
+      let s = (us as f64) / 1_000_000_f64;
+      let r = (self.count as f64) / s;
+      r
+    };
+
+    self.last_time = now;
+    self.count = 0;
+    return r;
+  }
+}
